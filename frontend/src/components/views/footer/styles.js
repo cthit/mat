a01350779aa8dffaf97be365ab286ef659500f43
portals <theodor.ang.98@gmail.com>
2018-05-18 23:58:26 +0200

    Made footer a p element instead of div

--- frontend/src/components/views/footer/styles.js
+++ frontend/src/components/views/footer/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 
-export const FooterContainer = styled.div`
+export const FooterContainer = styled.p`
     text-align:center;
     margin-top:16px;
+    margin-bottom:16px;
 `
