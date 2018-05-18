e1d96eb7db62f782b461be5b8b04d0aec4aa353e
portals <theodor.ang.98@gmail.com>
2018-05-19 00:07:24 +0200

    Improved pliancy

--- frontend/src/components/elements/rectangle_link/styles.js
+++ frontend/src/components/elements/rectangle_link/styles.js
@@ -11,9 +11,21 @@ export const Container = styled.div`
 
     text-align:center;
     background-color: white;
-    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+    box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.14),
+                    0 1px 10px 0 rgba(0, 0, 0, 0.12),
+                    0 2px 4px -1px rgba(0, 0, 0, 0.4);
+
+    &:hover{ 
+        box-shadow: 0 8px 10px 1px rgba(0, 0, 0, 0.14),
+                    0 3px 14px 2px rgba(0, 0, 0, 0.12),
+                    0 5px 5px -3px rgba(0, 0, 0, 0.4);
+    }
+
+    &:active{
+        box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
                     0 1px 5px 0 rgba(0, 0, 0, 0.12),
                     0 3px 1px -2px rgba(0, 0, 0, 0.2);
+    }
 `
 
 export const Link = styled(NavLink)`
