e3b8e454e1f80ccf31937d82acabafc8faaa9378
portals <theodor.ang.98@gmail.com>
2018-05-25 16:04:01 +0200

    Added env for backend url

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Router from './Router';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Router from "./Router";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<Router />, document.getElementById('root'));
+require("dotenv").config();
+
+ReactDOM.render(<Router />, document.getElementById("root"));
 registerServiceWorker();
