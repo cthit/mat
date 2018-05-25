1f9af32951ad07d736beb27f6afbc010ffacfec1
portals <theodor.ang.98@gmail.com>
2018-05-25 16:04:01 +0200

    Revert "Added env for backend url"
    This reverts commit e3b8e454e1f80ccf31937d82acabafc8faaa9378.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,76 +1,70 @@
-import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
-import axios from "axios";
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import axios from 'axios';
 
-import { DataContext } from "./components/context/DataContext";
+import { DataContext } from './components/context/DataContext';
 
-import HomeScreen from "./components/screens/home/index";
-import CategoryScreen from "./components/screens/category/index";
-import SushiMeScreen from "./components/screens/sushi_me/index";
-import SushiLauScreen from "./components/screens/sushi_lau/index";
+import HomeScreen from './components/screens/home/index';
+import CategoryScreen from './components/screens/category/index';
+import SushiMeScreen from './components/screens/sushi_me/index';
+import SushiLauScreen from './components/screens/sushi_lau/index';
 
 class App extends Component {
-  componentWillMount() {
-    this.setState({ categories: {} });
+  
+  componentWillMount(){
+    this.setState({categories: {}});
 
-    const backendUrl =
-      process.env.REACT_APP_BACKEND_URL == null
-        ? ""
-        : process.env.REACT_APP_BACKEND_URL;
+    axios.get('http://127.0.0.1:8080/api/mat.json')
+        .then(function(response){
+            var categories = {};
 
-    axios
-      .get(backendUrl + "/api/mat.json")
-      .then(
-        function(response) {
-          var categories = {};
+            for(var index in response.data){
+                var restaurantData = response.data[index];
+                if(categories[restaurantData.category] == null){
+                  categories[restaurantData.category] = [];
+                }
 
-          for (var index in response.data) {
-            var restaurantData = response.data[index];
-            if (categories[restaurantData.category] == null) {
-              categories[restaurantData.category] = [];
+                categories[restaurantData.category].push(restaurantData);
             }
 
-            categories[restaurantData.category].push(restaurantData);
-          }
-
-          this.setState({
-            categories: categories
-          });
-        }.bind(this)
-      )
-      .catch(function(error) {
-        console.log(error);
-      });
+            this.setState({
+              categories: categories
+            });
+            
+          }.bind(this))
+        .catch(function(error){
+            console.log(error);
+        });
   }
 
   render() {
     return (
       <div className="App">
         <DataContext.Provider value={this.state.categories}>
-          <Switch>
+  				<Switch>
             <Route path="/" exact>
-              <HomeScreen />
+              <HomeScreen/>
             </Route>
 
             <Route path="/menu/sushime" exact>
-              <SushiMeScreen />
+              <SushiMeScreen></SushiMeScreen>
             </Route>
 
             <Route path="/menu/sushilau" exact>
-              <SushiLauScreen />
+              <SushiLauScreen></SushiLauScreen>
             </Route>
 
             {Object.keys(this.state.categories).map(function(category, data) {
               const path = "/" + category;
               return (
                 <Route key={path} path={path} exact>
-                  <CategoryScreen key={category} category={category} />
+                  <CategoryScreen key={category} category={category}/>
                 </Route>
               );
             })}
-          </Switch>
+			  	</Switch>
         </DataContext.Provider>
-      </div>
+			</div>
     );
   }
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import Router from "./Router";
-import registerServiceWorker from "./registerServiceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Router from './Router';
+import registerServiceWorker from './registerServiceWorker';
 
-require("dotenv").config();
-
-ReactDOM.render(<Router />, document.getElementById("root"));
+ReactDOM.render(<Router />, document.getElementById('root'));
 registerServiceWorker();
