18017678b0df97e8419921bcaa5e8489e5941d6a
Theodor Angergård <portalprogramming@gmail.com>
2021-03-11 12:45:43 +0100

    Resolve issue where hidden failed validation

--- frontend/src/api/restaurants/get.restaurants.api.js
+++ frontend/src/api/restaurants/get.restaurants.api.js
@@ -2,7 +2,13 @@ import { getRequest } from "../utils/api.utils";
 
 export const getRestaurants = () => getRequest("/restaurants");
 
-export const getRestaurant = id => getRequest("/restaurants/" + id);
+export const getRestaurant = id =>
+    getRequest("/restaurants/" + id, response => ({
+        data: {
+            ...response.data,
+            hidden: response.data.hidden == null ? false : response.data.hidden
+        }
+    }));
 
 export const getAdminRestaurants = () => getRequest("/admin/restaurants");
 
